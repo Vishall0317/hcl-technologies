1dbbf1263e99d19acb05d014204ff0d2b30a1a7e
@VishalBramhankar <vishal.bramhankar@hcl.com>
2022-01-19 16:34:11 +0530

    All Exception Implementation are here.

--- src/main/java/com/hcl/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hcl/exception/GlobalExceptionHandler.java
@@ -2,14 +2,10 @@ package com.hcl.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
